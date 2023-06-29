ed2c5780009384b4f5e359f13e1501ab51c33475
Tomasz Warczykowski <tomasz.warczykowski@silksh.pl>
2023-06-29 13:58:56 +0200

    fix

--- target_group.tf
+++ target_group.tf
@@ -44,9 +44,12 @@ resource "aws_lb_listener_rule" "this" {
         var.service_dns_name
       ]
     }
-    dynamic "source_ip" {
-      for_each = length(var.source_ip) > 0 ? [1] : []
-      content {
+  }
+  
+  dynamic "condition" {
+    for_each = length(var.source_ip) > 0 ? [1] : []
+    content {
+      source_ip {
         values = var.source_ip
       }
     }
